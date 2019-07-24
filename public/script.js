60c38c6ab6c9ba402c958e5f971ef50abe66f30e
DuaaH <dodohalaiqa.1@gmail.com>
2019-07-24 11:18:54 +0300

    add function get and make request on server

---
+++ public/script.js
@@ -0,0 +1,36 @@
+const input = document.getElementById('search');
+
+const search = document.getElementById('submit');
+
+const req = (url, cb) => {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => cb(data))
+    .catch(error => error);
+};
+
+req('/search', (data) => {
+  if (input.value === '') {
+    alert('please enter the country');
+  }
+});
+
+//
+// search.addEventListener("click", event => {
+// if (input.value === "") {
+// alert("please enter the country");
+// } else {
+// input.textContent = "";
+// fetch(/search ${input.value})
+// .then(response => {
+// return response.json();
+// })
+// .then((data)=>{
+// data.forEach(element => {
+//
+//
+//
+//       });
+//   });
+// }
+// });
--- src/handler.js
+++ src/handler.js
@@ -1,50 +1,63 @@
-const path = require("path");
-const fs = require("fs");
-require("env2")(".env");
-const request = require("request");
+const path = require('path');
+const fs = require('fs');
+require('env2')('.env');
+const request = require('request');
+const getData = require('./queries/getData.js');
 
 const homePage = (req, res) => {
-  const homePagePath = path.join(__dirname, "..", "public/index.html");
-  fs.readFile(homePagePath, (err, data) => {
+  const homePagePath = path.join(__dirname, '..', 'public/index.html');
+
+  fs.readFile(homePagePath, (err, file) => {
     if (err) {
       res.writeHead(500, {
-        "Content-Type": "text/html"
+        'Content-Type': 'text/html',
       });
-      res.end("<h1>ERRORE</h1>");
+      res.end('<h1>ERRORE</h1>');
     }
     res.writeHead(200, {
-      "Content-Type": "text/html"
+      'Content-Type': 'text/html',
     });
-    res.end(data);
+    res.end(file);
   });
 };
 
 const publicHandler = (request, response, url) => {
-  const extension = url.split(".")[1];
+  const extension = url.split('.')[1];
   const extensionType = {
-    html: "text/html",
-    css: "text/css",
-    js: "application/javascript",
-    jpg: "image/jpg",
-    png: "image/png",
-    ico: "image/x-icon"
+    html: 'text/html',
+    css: 'text/css',
+    js: 'application/javascript',
+    jpg: 'image/jpg',
+    png: 'image/png',
+    ico: 'image/x-icon',
   };
-  const filePath = path.join(__dirname, "../public", url);
+  const filePath = path.join(__dirname, '../public', url);
   fs.readFile(filePath, (error, file) => {
     if (error) {
-      response.writeHead(500, { "Content-Type": "text/html" });
-      response.end("<h1>this is error message should be</h1>");
+      response.writeHead(500, { 'Content-Type': 'text/html' });
+      response.end('<h1>this is error message should be</h1>');
     } else {
-      response.writeHead(200, { "Content-Type": extensionType[extension] });
+      response.writeHead(200, { 'Content-Type': extensionType[extension] });
       response.end(file);
     }
+  });
+};
+const getTeamData = (request, response) => {
+  const { query } = url.parse(request.url);
+  const { text } = queryString.parse(query);
+  getData(text, (err, res) => {
+    if (err) return serverError(err, response);
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify(res));
   });
 };
 
 const notFound = (req, res) => {
   res.writeHead(404, {
-    "Content-Type": "text/html"
+    'Content-Type': 'text/html',
   });
-  res.end(`<h1>ERRORE 404 <br> Page Not Found</h1>`);
+  res.end('<h1>ERRORE 404 <br> Page Not Found</h1>');
 };
-module.exports = { homePage, publicHandler, notFound };
+module.exports = {
+  homePage, publicHandler, notFound, getTeamData,
+};
