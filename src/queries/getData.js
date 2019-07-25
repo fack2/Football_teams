2b390fc0ad7ea592b918e82cba3d6533acbbb288
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-07-25 10:55:16 +0300

    solve some of issus

--- src/queries/getData.js
+++ src/queries/getData.js
@@ -1,20 +1,10 @@
 const dbConnection = require("../database/db_connection");
 
 const getData = (country, cb) => {
-  if (country === "Spain") {
+  if (country) {
     dbConnection.query(
-      "select teams.logo , teams.description,teams.team_name, countries.country_name from countries inner join teams on teams.id_countries = countries.id_countries where countries.country_name='Spain'",
-      (err, res) => {
-        if (err) {
-          return cb(err);
-        }
-        console.log("res", res);
-        cb(null, res.rows);
-      }
-    );
-  } else if (country === "England") {
-    dbConnection.query(
-      "select teams.logo , teams.description,teams.team_name, countries.country_name from countries inner join teams on teams.id_countries = countries.id_countries where countries.country_name='England'",
+      "select teams.logo , teams.description,teams.team_name, countries.country_name from countries inner join teams on teams.id_countries = countries.id_countries where countries.country_name=$1",
+      [country],
       (err, res) => {
         if (err) {
           return cb(err);
--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
-const http = require('http');
-const router = require('./router.js');
+const http = require("http");
+const router = require("./router.js");
 
 const server = http.createServer(router);
 const port = process.env.PORT || 4002;
 
-const startServer = () => {
-  server.listen(port, () => {
-    console.log(`Magic happens on port ${port}`);
-  });
-};
-
-startServer();
+server.listen(port, () => {
+  console.log(`Magic happens on port ${port}`);
+});
